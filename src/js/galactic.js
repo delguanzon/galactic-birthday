0017d18eda8364180eb13f1f1ef23ae5308d8927
delguanzon <yodelguanzon@gmail.com>
2022-11-20 13:35:40 -0800

    Add new logic for validating the age and pass new tests

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,57 +1,60 @@
 export default class GalacticBirthCalc {
-  constructor(age){
-    if(age === undefined)
+  constructor(age) {
+    this.age = age;
+  }
+
+  validateAge() {
+    if (this.age === undefined)
       this.age = 0;
-    else if(isNaN(age))
+    if (isNaN(this.age))
       this.age = 'NaN';
-    else
-      this.age = age;
+    return this.age;
   }
 
-  getMercuryAge(){
-    return this.age*0.24;
+  getMercuryAge() {
+    return this.age * 0.24;
   }
 
-  getVenusAge(){
-    return this.age*0.62;
+  getVenusAge() {
+    return this.age * 0.62;
   }
 
-  getMarsAge(){
-    return this.age*1.88;
+  getMarsAge() {
+    return this.age * 1.88;
   }
 
-  getJupiterAge(){
-    return this.age*11.86;
+  getJupiterAge() {
+    return this.age * 11.86;
   }
 
   getZodiacExp(zodiac) {
-    const zodiacMap = new Map ([
-      ['aries',91],
-      ['taurus',87],
-      ['gemini',77],
-      ['cancer',55],
-      ['leo',67],
-      ['virgo',89],
-      ['libra',104],
-      ['scorpio',66],
-      ['sagittarius',65],
-      ['capricorn',101],
-      ['pisces',69],
-      ['aquarius',89],
+    const zodiacMap = new Map([
+      ['aries', 91],
+      ['taurus', 87],
+      ['gemini', 77],
+      ['cancer', 55],
+      ['leo', 67],
+      ['virgo', 89],
+      ['libra', 104],
+      ['scorpio', 66],
+      ['sagittarius', 65],
+      ['capricorn', 101],
+      ['pisces', 69],
+      ['aquarius', 89],
     ]);
     return zodiacMap.get(zodiac.toLowerCase());
   }
 
-  getYearsLeft(zodiac, planet){
+  getYearsLeft(zodiac, planet) {
     const planetName = planet.toLowerCase();
-    const planetMap = new Map ([
-      ['earth',1],
-      ['mercury',0.24],
-      ['venus',0.62],
-      ['mars',1.88],
-      ['jupiter',11.86]
+    const planetMap = new Map([
+      ['earth', 1],
+      ['mercury', 0.24],
+      ['venus', 0.62],
+      ['mars', 1.88],
+      ['jupiter', 11.86]
     ]);
-    if(this.age > this.getZodiacExp(zodiac)) {
+    if (this.age > this.getZodiacExp(zodiac)) {
       return Number(((this.age - this.getZodiacExp(zodiac)) * planetMap.get(planetName)).toFixed(2));
     }
     return Number(((this.getZodiacExp(zodiac) - this.age) * planetMap.get(planetName)).toFixed(2));
